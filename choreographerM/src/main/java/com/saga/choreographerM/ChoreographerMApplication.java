289b2235013f9bf70a2ed206e3f8d88475d06aa5
unknown <Ivan-Kokorin>
2023-05-04 17:43:36 +0300

    add 3 start-project

---
+++ choreographerM/src/main/java/com/saga/choreographerM/ChoreographerMApplication.java
@@ -0,0 +1,13 @@
+package com.saga.choreographerM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreographerMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoreographerMApplication.class, args);
+	}
+
+}
---
+++ choreographerM/src/test/java/com/saga/choreographerM/ChoreographerMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.choreographerM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreographerMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
